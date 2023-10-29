35a18a287d0fa05b57874f4a845a6968eb4b475d
malteusta <malteus.ta@gmail.com>
2023-10-29 15:41:00 -0300

    Dungeons + spawns

--- Assets/Scripts/Enemies/SpawnEnemies.cs
+++ Assets/Scripts/Enemies/SpawnEnemies.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawnEnemies : MonoBehaviour
 {
     public GameObject[] enemyPrefabs;
-    public Transform spawnPoint;
+    public Transform[] spawnPoints;
     public float spawnInterval;
 
     private void Start()
@@ -18,9 +18,11 @@ public class SpawnEnemies : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(spawnInterval);
-            int randomIndex = Random.Range(0, enemyPrefabs.Length);
-            GameObject enemyPrefab = enemyPrefabs[randomIndex];
-            Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+            int randomSpawnIndex = Random.Range(0, spawnPoints.Length);
+            Transform selectedSpawnPoint = spawnPoints[randomSpawnIndex];
+            int randomEnemyIndex = Random.Range(0, enemyPrefabs.Length);
+            GameObject enemyPrefab = enemyPrefabs[randomEnemyIndex];
+            Instantiate(enemyPrefab, selectedSpawnPoint.position, Quaternion.identity);
         }
     }
 }
